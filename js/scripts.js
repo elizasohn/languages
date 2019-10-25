1596ab77c60be002dd3904892f6c5a9b07d15a5a
Eliza Sohn <eliza.sohn@gmail.com>
2019-10-25 14:25:19 -0700

    add branches to show div based on first and fourth questions

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,100 @@ $(document).ready(function(){
   $("form").submit(function(event){
 
     var language1 = $("input:radio[name=language1]:checked").val();
-    var language2 = $("input:radio[name=language1]:checked").val();
-    var language3 = $("input:radio[name=language1]:checked").val();
-    var language4 = $("input:radio[name=language1]:checked").val();
-    var language5 = $("input:radio[name=language1]:checked").val();
+    var language2 = $("input:radio[name=language2]:checked").val();
+    var language3 = $("input:radio[name=language3]:checked").val();
+    var language4 = $("input:radio[name=language4]:checked").val();
+    var language5 = $("input:radio[name=language5]:checked").val();
 
-      if (language1 === "javascript" &&& language4 === "javascript"){
-        $(".javascript").show();
-        $(".ruby").hide();
-        $(".python").hide();
-        $(".swift").hide();
+      if (language1 === "javascript"){
+        if (language4 === "javascript"){
+          $(".javascript").show();
+          $(".ruby").hide();
+          $(".python").hide();
+          $(".swift").hide();
+        } else if (language4 === "ruby"){
+          $(".javascript").show();
+          $(".ruby").show();
+          $(".python").hide();
+          $(".swift").hide();
+        }else if (language4 === "python"){
+          $(".javascript").show();
+          $(".ruby").hide();
+          $(".python").show();
+          $(".swift").hide();
+        }else if (language4 === "swift"){
+          $(".javascript").show();
+          $(".ruby").hide();
+          $(".python").hide();
+          $(".swift").show();
+        }
       } else if (language1 === "ruby"){
-        $(".ruby").show();
+        if (language4 === "javascript"){
+          $(".javascript").show();
+          $(".ruby").show();
+          $(".python").hide();
+          $(".swift").hide();
+        }else if (language4 === "ruby"){
+          $(".javascript").hide();
+          $(".ruby").show();
+          $(".python").hide();
+          $(".swift").hide();
+        }else if (language4 === "python"){
+          $(".javascript").hide();
+          $(".ruby").show();
+          $(".python").show();
+          $(".swift").hide();
+        }else if (language4 === "swift"){
+          $(".javascript").hide();
+          $(".ruby").show();
+          $(".python").hide();
+          $(".swift").show();
+        }
       } else if (language1 === "python"){
-        $(".python").show();
+        if (language4 === "javascript"){
+          $(".javascript").show();
+          $(".ruby").hide();
+          $(".python").show();
+          $(".swift").hide();
+        }else if (language4 === "ruby"){
+          $(".javascript").hide();
+          $(".ruby").show();
+          $(".python").show();
+          $(".swift").hide();
+        }else if (language4 === "python"){
+          $(".javascript").hide();
+          $(".ruby").hide();
+          $(".python").show();
+          $(".swift").hide();
+        }else if (language4 === "swift"){
+          $(".javascript").hide();
+          $(".ruby").hide();
+          $(".python").show();
+          $(".swift").show();
+        }
       } else if (language1 === "swift"){
-        $(".swift").show();
+        if (language4 === "javascript"){
+          $(".javascript").show();
+          $(".ruby").hide();
+          $(".python").hide();
+          $(".swift").show();
+        }else if (language4 === "ruby"){
+          $(".javascript").hide();
+          $(".ruby").show();
+          $(".python").hide();
+          $(".swift").show();
+        }else if (language4 === "python"){
+          $(".javascript").hide();
+          $(".ruby").hide();
+          $(".python").show();
+          $(".swift").show();
+        }else if (language4 === "swift"){
+          $(".javascript").hide();
+          $(".ruby").hide();
+          $(".python").hide();
+          $(".swift").show();
+        }
       }
-
     event.preventDefault();
   });
 });
