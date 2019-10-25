f5d43b6fbf1596065316180051cd672055034f7c
Eliza Sohn <eliza.sohn@gmail.com>
2019-10-25 12:30:47 -0700

    add button, add another question, add js to capture form info, add branches for first quiz question to test, add styling to background of quiz div

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
 $(document).ready(function(){
-  $("form").submit(function())
-}
+  $("form").submit(function(event){
+
+    var language1 = $("input:radio[name=language1]:checked").val();
+      if (language1 === "javascript"){
+        $(".javascript").show();
+      } else if (language1 === "ruby"){
+        $(".ruby").show();
+      } else if (language1 === "python"){
+        $(".python").show();
+      } else if (language1 === "swift"){
+        $(".swift").show();
+      }
+
+    event.preventDefault();
+  });
+});
 
 
 
 
 
 
-if ()
+//add button
+//add another question
+//add js to capture form info
