66db71fe6b92c7ec20f6d9d12b0ffdfea00e692c
Eliza Sohn <eliza.sohn@gmail.com>
2019-10-25 13:59:19 -0700

    add variables for each radio form, add some branches

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,16 @@ $(document).ready(function(){
   $("form").submit(function(event){
 
     var language1 = $("input:radio[name=language1]:checked").val();
-      if (language1 === "javascript"){
+    var language2 = $("input:radio[name=language1]:checked").val();
+    var language3 = $("input:radio[name=language1]:checked").val();
+    var language4 = $("input:radio[name=language1]:checked").val();
+    var language5 = $("input:radio[name=language1]:checked").val();
+
+      if (language1 === "javascript" &&& language4 === "javascript"){
         $(".javascript").show();
+        $(".ruby").hide();
+        $(".python").hide();
+        $(".swift").hide();
       } else if (language1 === "ruby"){
         $(".ruby").show();
       } else if (language1 === "python"){
